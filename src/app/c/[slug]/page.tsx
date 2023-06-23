db2a14023a67b3bcf2043cd5cd4ae2ae807ba7f5
Shamanth <shamanth1604@gmail.com>
2023-06-23 19:59:20 +0530

    Community page header

---
+++ src/app/c/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+interface CommunityPageProps {
+    params: {
+        slug: string;
+    };
+}
+
+export default async function CommunityPage({ params }: CommunityPageProps) {
+    const { slug } = params;
+    const session = await getAuthSession();
+
+    const community = await db.community.findFirst({
+        where: {
+            name: slug,
+        },
+        include: {
+            posts: {
+                include: {
+                    author: true,
+                    votes: true,
+                    comments: true,
+                    community: true,
+                },
+                take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+            },
+        },
+    });
+
+    if (!community) {
+        return notFound();
+    }
+
+    return (
+        <>
+            <h1 className="font-bold text-3xl md:text-4xl h-14">
+                c/{community.name}
+            </h1>
+            <MiniCreatePost session={session} />
+        </>
+    );
+}
---
+++ src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,5 @@
+const SubscribeLeaveToggle = () => {
+    return <div></div>;
+};
+
+export default SubscribeLeaveToggle;
